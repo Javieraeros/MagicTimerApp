70dd5c8b3abf343d8d6412332baffd07697e9cac
Javi Ruiz <fjruiz.1992@gmail.com>
2025-10-26 16:25:43 +0100

    [MIAND-1] Add counter card

--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -39,12 +39,18 @@ dependencies {
     implementation(libs.androidx.appcompat)
 
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling)
     implementation(libs.androidx.ui.tooling.preview)
 
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons)
+
+    //Images
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
